a3a54270d537556363299d5a89e0d2e72fccaf1d
Steven <syhoung1@gmail.com>
2017-06-09 21:48:20 +0000

    adding left off files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605083543) do
+ActiveRecord::Schema.define(version: 20170609205648) do
 
   create_table "items", force: :cascade do |t|
     t.string   "todo_item"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170605083543) do
     t.datetime "updated_at", null: false
     t.string   "email"
     t.string   "password"
+    t.string   "username"
   end
 
 end
